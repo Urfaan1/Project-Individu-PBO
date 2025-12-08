b892bf770493fa155bb05edb8b3725ad5fd3b5f7
urfaan <urfanhusaini@gmail.com>
2025-12-08 17:34:33 +0700

    Implement constructor and getters in Penumpang class
    Added constructor and getter methods for Penumpang class.

--- Penumpang.java
+++ Penumpang.java
@@ -4,3 +4,28 @@ public class Penumpang {
     private boolean hamil; 
     private int saldo; 
     private static final int SALDO_AWAL = 10000; 
+     /**
+     * Constructor untuk Class Penumpang.
+     * Saldo awal diatur ke SALDO_AWAL (10.000).
+     * @param id ID unik penumpang
+     * @param umur Umur penumpang
+     * @param hamil Status hamil penumpang
+     */
+    public Penumpang(int id, int umur, boolean hamil) {
+        this.id = id;
+        this.umur = umur;
+        this.hamil = hamil;
+        this.saldo = SALDO_AWAL; 
+    }
+    public int getID() {
+        return id;
+    }
+    public int getUmur() {
+        return umur;
+    }
+    public boolean getHamil() {
+        return hamil;
+    }
+    public int getSaldo() {
+        return saldo;
+    }
